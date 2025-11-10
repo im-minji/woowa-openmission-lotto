d6ffd488536a65f937a8456845156202a8d12860
“im-minji” <mjlim514@gmail.com>
2025-11-10 12:38:32 +0900

    feat: repositry 구현

--- src/main/java/com/woowa/lotto/repository/MyLottoRepository.java
+++ src/main/java/com/woowa/lotto/repository/MyLottoRepository.java
@@ -1,4 +1,9 @@
 package com.woowa.lotto.repository;
 
-public class MyLottoRepository {
+import com.woowa.lotto.domain.MyLotto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyLottoRepository extends JpaRepository<MyLotto, Long> {
 }
--- src/main/java/com/woowa/lotto/repository/PurchasedLottoRepository.java
+++ src/main/java/com/woowa/lotto/repository/PurchasedLottoRepository.java
@@ -1,4 +1,12 @@
 package com.woowa.lotto.repository;
 
-public class PurchasedLottoRepository {
+import com.woowa.lotto.domain.PurchasedLotto;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchasedLottoRepository extends JpaRepository<PurchasedLotto, Long> {
+    List<PurchasedLotto> findAllByPurchaseDateBetween(LocalDate start, LocalDate end);
 }
