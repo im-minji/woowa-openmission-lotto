c2485052bbd1d75c1421ff4965bbcbd01570e67f
“im-minji” <mjlim514@gmail.com>
2025-11-10 11:44:43 +0900

    feat: MyLotto 객체에 Annotation 추가

--- src/main/java/com/woowa/lotto/domain/MyLotto.java
+++ src/main/java/com/woowa/lotto/domain/MyLotto.java
@@ -1,10 +1,25 @@
 package com.woowa.lotto.domain;
 
 import java.util.Objects;
+import jakarta.persistence.*;
 
+@Entity
 public class MyLotto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL) // MyLotto가 저장/삭제될 때, Lotto도 같이 저장/삭제
+    @JoinColumn(name = "lotto_id")
     private final Lotto myLotto;
+
+    @Column
     private final String lottoName;
+
+    protected MyLotto() {
+        this.myLotto = null;
+        this.lottoName = null;
+    }
 
     public MyLotto(Lotto myLotto, String lottoName) {
         this.myLotto = Objects.requireNonNull(myLotto, "[ERROR] 로또가 비워져 있습니다.");
@@ -22,6 +37,9 @@ public class MyLotto {
     }
 
     public Lotto getMyLotto() {return myLotto;}
+
     public String getLottoName() {return lottoName;}
+
+    public Long getId() {return id;}
 }
 
