1159176977e99a0a77b2d79f5c7bfda25c8733e4
“im-minji” <mjlim514@gmail.com>
2025-11-10 11:54:14 +0900

    feat: WinningLotto 객체에 Annotation 추가 및 bonusNum 변수 Integer로 수정

--- src/main/java/com/woowa/lotto/domain/WinningLotto.java
+++ src/main/java/com/woowa/lotto/domain/WinningLotto.java
@@ -1,14 +1,32 @@
 package com.woowa.lotto.domain;
 
 import java.time.LocalDate;
+import jakarta.persistence.*;
+import java.util.Objects;
 
+@Entity
 public class WinningLotto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     // 당첨 번호를 가지는 객체 (6개 번호 + 1개의 보너스 번호)
+    @OneToOne(cascade = CascadeType.ALL)
     private final Lotto winningLotto;
-    private final int bonusNum;
+
+    @Column
+    private final Integer bonusNum;
+
+    @Column
     private final LocalDate drawDate;
 
-    public WinningLotto(Lotto winningLotto, int bonusNum, LocalDate drawDate) {
+    protected WinningLotto() {
+        this.winningLotto = null;
+        this.bonusNum = null;
+        this.drawDate = null;
+    }
+
+    public WinningLotto(Lotto winningLotto, Integer bonusNum, LocalDate drawDate) {
         validate(winningLotto, bonusNum);
         this.winningLotto = winningLotto;
         this.bonusNum = bonusNum;
@@ -16,7 +34,9 @@ public class WinningLotto {
     }
 
     // 보너스 번호를 가지는 객체이므로 보너스 번호에 대한 검증은 winningLotto가 해야함
-    private void validate(Lotto winningLotto, int bonusNum) {
+    private void validate(Lotto winningLotto, Integer bonusNum) {
+        Objects.requireNonNull(bonusNum, "[ERROR] 보너스 번호는 null일 수 없습니다.");
+
         if(bonusNum < 1 || bonusNum > 45) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 1~45여야 합니다.");
         }
@@ -29,8 +49,10 @@ public class WinningLotto {
     // 서비스에서 보너스 번호, 당첨번호, 만든 날짜를 사용하기 위한 getter
     public Lotto getWinningLotto() {return winningLotto;}
 
-    public int getBonusNum() {return bonusNum;}
+    public Integer getBonusNum() {return bonusNum;}
 
     public LocalDate getDrawDate() {return drawDate;}
+
+    public Long getId() {return id;}
 
 }
